ba5001553d9a0b69c746931c54343b69d8159ab6
Like <likesoapiya@ukr.net>
2025-12-13 11:38:02 +0200

    inspect random module

--- Lesson_5.4.py
+++ Lesson_5.4.py
@@ -1,7 +1,29 @@
 import random
 import sys
 import requests
+import inspect
+
+otstup = f"{'':-^50}"
+print(otstup)
+
+print(inspect.ismodule(random))
+print(inspect.isclass(random))
+print(inspect.isfunction(random))
+
+print(otstup)
+
+print(inspect.getmodule(random))
+print(inspect.getmodule(list))
+
+print(otstup)
+
+print(random.getstate)
+print(random.getrandbits)
+
+print(otstup)
 
 for module_name, module_path in sys.modules.items():
     name_of_module = f"{module_name:^50}"
     print(name_of_module, module_path)
+
+print(otstup)
