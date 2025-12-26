701d15a7f171c64a845aefb8ac0f9ff1e0d42a78
nekstenur <nekstentv@gmail.com>
2025-12-26 19:40:06 +0200

    Dz 5, colorama module inspect

--- Lesson_5.4.py
+++ Lesson_5.4.py
@@ -1,26 +1,20 @@
-import random
+import colorama
 import sys
-import requests
 import inspect
 
 otstup = f"{'':-^50}"
 print(otstup)
 
-print(inspect.ismodule(random))
-print(inspect.isclass(random))
-print(inspect.isfunction(random))
+print(inspect.ismodule(colorama))
+print(inspect.isclass(colorama))
+print(inspect.isfunction(colorama))
 
 print(otstup)
 
-print(inspect.getmodule(random))
+print(inspect.getmodule(colorama))
 print(inspect.getmodule(list))
 
 print(otstup)
-
-print(random.getstate)
-print(random.getrandbits)
-
-print(otstup) #das
 
 for module_name, module_path in sys.modules.items():
     name_of_module = f"{module_name:^50}"
